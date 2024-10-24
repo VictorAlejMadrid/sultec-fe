cf0a03460fa2f0b09ccb7aa11ad132af1e730efe
victor.madrid <victor.madrid@vortigo.com.br>
2024-10-24 14:40:41 -0300

    💄 New table scroll with shadcn

--- app/dashboard/clients/_components/ClientTable/index.tsx
+++ app/dashboard/clients/_components/ClientTable/index.tsx
@@ -5,6 +5,7 @@ import { ReadClientDto } from '@/interfaces/ClientDtos/ReadClientDto';
 import { PaginatedResult, Pagination as PaginationType } from '@/interfaces/result';
 import { UseMutateFunction } from '@tanstack/react-query';
 import PaginationComponent from '@/components/Pagination';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 interface IClientTable {
   clients: PaginatedResult<ReadClientDto>;
@@ -17,11 +18,13 @@ export default function ClientTable(props: IClientTable) {
 
   return (
     <>
-      <div className="w-full flex flex-col flex-1 gap-2 overflow-y-auto scroll-smooth">
-        {clients.data.map((client) => {
-          return <ClientCard key={client.id} client={client} />;
-        })}
-      </div>
+      <ScrollArea className="w-full flex flex-col flex-1 gap-2 overflow-y-auto scroll-smooth">
+        <div className="flex flex-col gap-2">
+          {clients.data.map((client) => {
+            return <ClientCard key={client.id} client={client} />;
+          })}
+        </div>
+      </ScrollArea>
 
       <PaginationComponent mutate={mutate} pagination={pagination} />
     </>
