7896f9d174869fc0e0d162c95c40356e38fd6d4e
victor.madrid <victor.madrid@vortigo.com.br>
2024-10-25 12:16:41 -0300

    🚧 First page is under construction

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div className="bg-black h-full w-full">To be decided</div>;
+  return (
+    <div className="h-full w-full flex flex-col items-center">
+      To be decided
+      <Link href="dashboard/clients">
+        <Button variant="sultec">Go to dashboards/clients</Button>
+      </Link>
+    </div>
+  );
 }
