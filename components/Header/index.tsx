e230a39faac6aba262d6a04ca4cbb83314b9197c
victor.madrid <victor.madrid@vortigo.com.br>
2024-10-25 17:38:24 -0300

    🚸 💄 Navigation bar in the header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,8 +4,14 @@ import Image from 'next/image';
 import LogoSultecWhite from '@/assets/images/LogoSultecWhite.svg';
 import UserMenu from './UserMenu';
 import ThemeToggle from './ThemeToggle';
+import { HouseIcon, MapPin, UsersIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import SimpleTooltip from '../SimpleTooltip';
+import Link from 'next/link';
 
 export default function Header() {
+  const IconStyle = cn('hover:scale-125 transition-all hover:cursor-pointer');
+
   return (
     <header className="z-40 w-full h-[80px] bg-blue-700 flex items-center justify-between px-5 py-2">
       <Image
@@ -15,6 +21,25 @@ export default function Header() {
         src={LogoSultecWhite}
         width={200}
       />
+
+      <nav className="flex items-center text-white gap-4">
+        <SimpleTooltip text="Serviços">
+          <Link href="/">
+            <HouseIcon size={30} className={IconStyle} />
+          </Link>
+        </SimpleTooltip>
+        <SimpleTooltip text="Clientes">
+          <Link href="/dashboard/clients">
+            <UsersIcon size={30} className={IconStyle} />
+          </Link>
+        </SimpleTooltip>
+        <SimpleTooltip text="Endereços">
+          <Link href="dashboard/addresses">
+            <MapPin size={30} className={IconStyle} />
+          </Link>
+        </SimpleTooltip>
+      </nav>
+
       <div className="flex flex-row gap-3 items-center">
         <ThemeToggle />
         <UserMenu />
